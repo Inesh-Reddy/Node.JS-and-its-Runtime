38ceb2652b38974f5f31ac7b89ae9a9d5639df9d
Inesh-Reddy <ineshreddy007@gmail.com>
2024-06-14 10:36:38 +0530

    Simple testing of routes

--- Http-Server/index.js
+++ Http-Server/index.js
@@ -4,10 +4,19 @@ const express = require("express");
 const app = express();
 const port  = 3000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello from the backend!!");
-})
 
+// middlewares
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send('<b>Hello From Backend<b>');
+})
+app.post(('/conversations'), (req,res)=>{
+    console.log(req.query.name);
+    res.status(200).json({
+        msg: '2+2 = 4',
+    })
+})
 app.listen(port, ()=>{
     console.log(`Server is listening on port : ${port} ...`);
 })
